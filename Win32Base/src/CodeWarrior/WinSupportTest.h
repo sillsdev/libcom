ebb483bfef573db02808dc1e3eaee7235f0db3e8
Graeme Costin <gcostin@mac.com>
2002-11-09 02:11:12 +0000

    Conditional compilation for testing version of WinSupportLib. WinSupportLib v1.3
    svn-revision: 10

--- Win32Base/src/CodeWarrior/WinSupportTest.h
+++ Win32Base/src/CodeWarrior/WinSupportTest.h
@@ -1,17 +1,20 @@
-//	WinSupportTesting.h
+//	WinSupportTest.h
 //
 //	Prefix file for testing Carbon target of WinSupport library
 //
+//	Removed _assert #define for CW 8
+//		2002-08-07, GDLC
 //	#define TESTING 1 added.
 //		2002-06-21, GDLC
 //	_MAC changed to MAC.
 //		2002-03-21, GDLC
 //	Written, Graeme Costin, 2001-08-21
+//
+//	$Id$
 
 #include <MacHeadersCarbon.h>
 
 #define _DEBUG
-#define _assert __assertion_failed
 
 #define	TESTING 1
 
