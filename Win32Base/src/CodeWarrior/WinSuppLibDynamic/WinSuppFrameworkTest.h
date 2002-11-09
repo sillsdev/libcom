f130b45f0b0a9c0475bbe4a63131212f4be6dd49
Graeme Costin <gcostin@mac.com>
2002-11-09 08:56:42 +0000

    WinSupportLib v1.3
    svn-revision: 13

---
+++ Win32Base/src/CodeWarrior/WinSuppLibDynamic/WinSuppFrameworkTest.h
@@ -0,0 +1,15 @@
+//	WinSuppFrameworkTest.h
+//
+//	Prefix file for debugging Mach-O target of WinSupport library
+//
+//	Written, Graeme Costin, 2002-09-19
+//
+//	$Id$
+
+#include <MSL MacHeadersMach-O.h>
+
+#define _DEBUG
+
+#define TESTING 1
+
+#define	MAC
