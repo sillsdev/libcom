52513c9d7bbf9b9cdebe70f3b5a08c903e7d8356
Graeme Costin <gcostin@mac.com>
2003-01-26 23:45:26 +0000

    Substitute for windows.h in a ported windows application
    svn-revision: 15

---
+++ Win32Base/include/windows.h
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------------------------
+Copyright 2002, SIL International. All rights reserved.
+
+File: windows.h
+Responsibility: Graeme Costin
+Reviewed:	Date		Reviewer
+			never
+
+	$Id$
+			
+Header file to include all the definitions supplied in WinSupportLib and COMSupportLib
+for applications ported from windows which expect the necessary Windows definitions to
+be obtained from windows.h
+	2002-12-09, GDLC
+----------------------------------------------------------------------------------------------*/
+
+#include "WinSupport.h"
+
+#include "COMSupport.h"
