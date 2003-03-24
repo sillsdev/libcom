662f5cfa219e0f0a01e71126a71f55ec31b907f3
Graeme Costin <gcostin@mac.com>
2003-03-24 02:54:01 +0000

    First compile of Mach-O form
    svn-revision: 19

---
+++ Win32Base/test/WinSuppLibTest/WinSuppLibTestDebugMach-O.h
@@ -0,0 +1,15 @@
+//	WinSuppLibTestMach-O.h
+//
+//	Prefix file for debugging Mach-O target of WinSupportLib test program
+//
+//	Written, Graeme Costin, 2003-03-21
+//
+//	$Id$
+
+#include <MSL MacHeadersMach-O.h>
+
+#define _DEBUG
+
+#define TESTING 1
+
+#define	MAC
---
+++ Win32Base/test/WinSuppLibTest/WinSuppLibTestFinalMach-O.h
@@ -0,0 +1,11 @@
+//	WinSuppLibTestFinalMach-O.h
+//
+//	Prefix file for testing Mach-O target of WinSupportLib test program
+//
+//	Written, Graeme Costin, 2003-03-21
+//
+//	$Id$
+
+#include <MSL MacHeadersMach-O.h>
+
+#define	MAC
