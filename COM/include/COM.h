91cadf87561b2e50b9cad55026a93af37a854142
Neil Mayhew <neil_mayhew@sil.org>
2006-08-10 22:18:13 +0000

    Add CLSCTX definitions
    svn-revision: 33

--- COM/include/COM.h
+++ COM/include/COM.h
@@ -15,10 +15,10 @@
 
 struct GUID
 {
-    unsigned long  Data1;
-    unsigned short Data2;
-    unsigned short Data3;
-    unsigned char  Data4[8];
+	unsigned long  Data1;
+	unsigned short Data2;
+	unsigned short Data3;
+	unsigned char  Data4[8];
 	
 	// These are not in Win32, but we need them to make our emulation work
 	GUID();
@@ -48,6 +48,18 @@ extern GUID IID_NULL;
 
 #define DECLSPEC_UUID(X) // Can't be done
 #define MIDL_INTERFACE(X) DECLSPEC_UUID(X) interface
+
+enum CLSCTX
+{
+	CLSCTX_INPROC_SERVER  = 1,
+	CLSCTX_INPROC_HANDLER = 2,
+	CLSCTX_LOCAL_SERVER   = 4,
+	CLSCTX_REMOTE_SERVER  = 8,
+};
+
+#define CLSCTX_INPROC (CLSCTX_INPROC_SERVER|CLSCTX_INPROC_HANDLER)
+#define CLSCTX_SERVER (CLSCTX_INPROC_SERVER|CLSCTX_LOCAL_SERVER)
+#define CLSCTX_ALL    (CLSCTX_INPROC|CLSCTX_SERVER)
 
 BSTR SysAllocStringLen(const OLECHAR* pch, unsigned int cch);
 BSTR SysAllocString(const OLECHAR *sz); // Zero-terminated
