b7dff1b115e5277b8dde78183ba7fa9dfa68b7b5
Brent McCarthy <brent_mccarthy@sil.org>
2006-08-29 17:12:51 +0000

    Text Services interface definitions and types
    svn-revision: 64

--- Win32Base/include/msctf.h
+++ Win32Base/include/msctf.h
@@ -1,18 +1,64 @@
 typedef LONG TS_STATUS;
 typedef LONG TS_SELECTION_ACP;
 typedef LONG TS_RUNINFO;
-typedef LONG TS_TEXTCHANGE;
-typedef LONG TS_TEXTCHANGE;
 typedef LONG TS_ATTRVAL;
 typedef LONG TS_ATTRID;
-typedef LONG TS_TEXTCHANGE;
-typedef LONG TS_TEXTCHANGE;
+
+typedef struct {
+  LONG acpStart;
+  LONG acpOldEnd;
+  LONG acpNewEnd;
+} TS_TEXTCHANGE;
 
 typedef DWORD TsViewCookie;
 typedef DWORD TfClientId;
 typedef DWORD TfEditCookie;
 
 enum { TS_LF_READWRITE, TS_LC_CHANGE };
+	
+typedef enum 
+{
+  TF_CT_NONE = 0, 
+  TF_CT_SYSCOLOR = 1, 
+  TF_CT_COLORREF = 2
+} TF_DA_COLORTYPE;
+
+typedef struct {
+  TF_DA_COLORTYPE type;
+  union {
+    int nIndex;
+    COLORREF cr;
+  };
+} TF_DA_COLOR;
+
+typedef enum 
+{
+  TF_LS_NONE = 0, 
+  TF_LS_SOLID = 1, 
+  TF_LS_DOT = 2, 
+  TF_LS_DASH = 3, 
+  TF_LS_SQUIGGLE = 4
+} TF_DA_LINESTYLE;
+
+typedef enum 
+{
+  TF_ATTR_INPUT = 0, 
+  TF_ATTR_TARGET_CONVERTED = 1, 
+  TF_ATTR_CONVERTED = 2, 
+  TF_ATTR_TARGET_NOTCONVERTED = 3, 
+  TF_ATTR_INPUT_ERROR = 4, 
+  TF_ATTR_FIXEDCONVERTED = 5, 
+  TF_ATTR_OTHER = -1
+} TF_DA_ATTR_INFO;
+
+typedef struct {
+  TF_DA_COLOR crText;
+  TF_DA_COLOR crBk;
+  TF_DA_LINESTYLE lsStyle;
+  BOOL fBoldLine;
+  TF_DA_COLOR crLine;
+  TF_DA_ATTR_INFO bAttr;
+} TF_DISPLAYATTRIBUTE;
 
 class ITextStoreACP : public IUnknown
 {
@@ -22,6 +68,9 @@ class ITextStoreACPSink : public IUnknown
 {
 public:
 	HRESULT OnLayoutChange(int, TsViewCookie&);
+	HRESULT OnLockGranted(DWORD);
+	HRESULT OnTextChange(DWORD, TS_TEXTCHANGE*);
+	HRESULT OnSelectionChange();
 };
 
 class ITextStoreACPServices : public IUnknown
@@ -30,6 +79,8 @@ class ITextStoreACPServices : public IUnknown
 
 class ITfThreadMgr : public IUnknown
 {
+public:
+	HRESULT Deactivate();
 };
 
 class ITfDocumentMgr : public IUnknown
@@ -46,6 +97,8 @@ class ITfCategoryMgr : public IUnknown
 
 class ITfDisplayAttributeMgr : public IUnknown
 {
+public:
+	HRESULT GetDisplayAttributeInfo(REFGUID, ITfDisplayAttributeInfo**, CLSID*);
 };
 
 class ITfProperty : public IUnknown
@@ -66,14 +119,21 @@ class ITfRange : public IUnknown
 
 class ITfRangeACP : public IUnknown
 {
+public:
+	HRESULT GetExtent(LONG*, LONG*);
 };
 
 class ITfDisplayAttributeInfo : public IUnknown
 {
+public:
+	HRESULT GetAttributeInfo(TF_DISPLAYATTRIBUTE*);
+	
 };
 
 class ITfMouseSink : public IUnknown
 {
+public:
+	HRESULT OnMouseEvent(ULONG, ULONG, DWORD, BOOL*);	
 };
 
 class ITfContextOwnerCompositionSink : public IUnknown
