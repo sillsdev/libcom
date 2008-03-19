ac93a507115fc926332f9f18dcb4edb0f73e1a90
MarkS <marksvc@gmail.com>
2008-03-19 23:52:46 +0000

    Added header+copyright block.
    svn-revision: 308

--- Win32Base/include/BasicTypes.h
+++ Win32Base/include/BasicTypes.h
@@ -3,7 +3,26 @@
  *
  *	Main type names used in Win32 code
  *
- *	Neil Mayhew - Apr 22, 2005
+ *	Neil Mayhew - 2005-04-22
+ *
+ * Win32 Compatibility Library
+ * Copyright (C) 2008 SIL International
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * http://www.gnu.org/licenses/lgpl.html
  */
 
 #ifndef _BASICTYPES_H_
