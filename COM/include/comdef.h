88c6869fd3e554bb7f1bffdd84a7f8c5efd064f8
MarkS <marksvc@gmail.com>
2008-03-20 21:04:56 +0000

    initial checkin
    svn-revision: 310

---
+++ COM/include/comdef.h
@@ -0,0 +1,30 @@
+/*
+ *	$Id$
+ *
+ *	This file is for compatibility with the naming and use of Windows header files.
+ *
+ *	MarkS - 2008-03-19
+ *
+ * COM Support Library
+ * Copyright (C) 2007 SIL International
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+
+#include "Ole2.h"
+#include "BasicTypes.h"
+#include "COMInterfaces.h"
