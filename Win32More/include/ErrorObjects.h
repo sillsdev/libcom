1759448c5074d441ef8bbbe05ace6ba1cabd7512
Tom Hindle <tom_hindle@sil.org>
2008-06-11 16:36:19 +0000

    Initial Checking of ErrorObjects.h
    svn-revision: 359

---
+++ Win32More/include/ErrorObjects.h
@@ -0,0 +1,81 @@
+#pragma once
+#ifndef _ErrorObjects_H__
+#define _ErrorObjects_H__
+
+#include <COMInterfacesMore.h>
+
+#include "WinError.h"
+
+/* namesapce ErrorInfo provides implementations for ICreateErrorInfo and IErrorInfo
+ *
+ */
+namespace ErrorObjects
+{
+
+/* class ErrorInfo basic implementation of IErrorInfo and CreateErrorInfo
+ */
+class ErrorInfo : public IErrorInfo, public ICreateErrorInfo
+{
+public:
+	
+	// default construtor
+	ErrorInfo();
+
+	// virual destructor
+	virtual ~ErrorInfo();
+
+	// IErrorInfo methods
+	virtual HRESULT STDMETHODCALLTYPE GetGUID(
+		/* [out] */ GUID *pGUID);
+
+	virtual HRESULT STDMETHODCALLTYPE GetSource(
+		/* [out] */ BSTR *pBstrSource);
+
+	virtual HRESULT STDMETHODCALLTYPE GetDescription(
+		/* [out] */ BSTR *pBstrDescription);
+
+	virtual HRESULT STDMETHODCALLTYPE GetHelpFile(
+		/* [out] */ BSTR *pBstrHelpFile);
+
+	virtual HRESULT STDMETHODCALLTYPE GetHelpContext(
+		/* [out] */ DWORD *pdwHelpContext);
+
+	// ICreateErrorInfo methods
+	virtual HRESULT STDMETHODCALLTYPE SetGUID(
+		/* [in] */ REFGUID rguid);
+		
+	virtual HRESULT STDMETHODCALLTYPE SetSource(
+		/* [in] */ LPOLESTR szSource);
+		
+	virtual HRESULT STDMETHODCALLTYPE SetDescription(
+		/* [in] */ LPOLESTR szDescription);
+		
+	virtual HRESULT STDMETHODCALLTYPE SetHelpFile(
+		/* [in] */ LPOLESTR szHelpFile);
+		
+	virtual HRESULT STDMETHODCALLTYPE SetHelpContext(
+		/* [in] */ DWORD dwHelpContext);
+
+	// IUknown Methods
+        virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void ** ppv);
+        virtual ULONG   STDMETHODCALLTYPE AddRef();
+        virtual ULONG   STDMETHODCALLTYPE Release();
+
+	
+protected:
+
+	// IErrorInfo Data members
+
+	GUID m_GUID;
+	BSTR m_BstrSource;
+	BSTR m_BstrDescript;
+	BSTR m_BstrHelpFile;
+	DWORD m_dwHelpContext;
+
+	// reference counter
+	int m_cref;
+};
+
+}
+
+#endif
