27731e27545c435a7508c64e04e1155d6207cb43
MarkS <marksvc@gmail.com>
2008-07-07 21:47:31 +0000

    initial commit
    svn-revision: 370

---
+++ Win32Base/include/winuser.h
@@ -0,0 +1,47 @@
+/*
+ *      $Id$
+ *
+ *      Definitions
+ *
+ *      MarkS - 2008-07-07
+ *
+ * Win32 Compatibility Library
+ * Copyright (C) 2008 SIL International
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+
+#ifndef win32base_winuser_h
+#define win32base_winuser_h
+
+// Standard dialog button IDs
+enum {
+	IDOK = 1,
+	IDCANCEL = 2,
+	IDABORT = 3,
+	IDRETRY = 4,
+	IDIGNORE = 5,
+	IDYES = 6,
+	IDNO = 7,
+	IDCLOSE = 8,
+	IDHELP = 9,
+	IDTRYAGAIN = 10,
+	IDCONTINUE = 11
+}
+
+#endif // !win32base_winuser_h
+
