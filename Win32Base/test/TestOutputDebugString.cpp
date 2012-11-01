50e99ae3a1964f6b3d00278f6312c02ff7601722
Neil Mayhew <neil_mayhew@sil.org>
2012-11-01 17:47:26 -0600

    Improve OutputDebugString unit test
    Make TestOutputDebugString check its own output
    
    Change-Id: Idc3ad2184d4f09a0695f0ac5d1ca5a8ec048c828

--- Win32Base/test/TestOutputDebugString.cpp
+++ Win32Base/test/TestOutputDebugString.cpp
@@ -26,12 +26,118 @@
 //
 
 #include "Hacks.h"
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/stat.h>
+
+bool equal(std::string name, FILE* f, std::string expected);
 
 int main(int argc, char** argv)
 {
+	int failures = 0;
+
+	// Capture stdout/stderr
+
+	FILE* outfile = tmpfile();
+	FILE* errfile = tmpfile();
+	int saved_stdout = dup(STDOUT_FILENO);
+	int saved_stderr = dup(STDERR_FILENO);
+
+	std::cout.flush();
+	std::cerr.flush();
+	fflush(stdout);
+	fflush(stderr);
+
+	dup2(fileno(outfile), STDOUT_FILENO); // Redirect stdout
+	dup2(fileno(errfile), STDERR_FILENO); // Redirect stderr
+
+	// Run tests
+
 	// \u2022 == \xE2\x80\xA2 == bullet, \u00E9 == e acute
 	OutputDebugString(L"Wide\u2022Wide\u2022Wide\n");
 	OutputDebugString("Narrow\xE2\x80\xA2Narrow\xe2\x80\xa2Narrow\n");
 	OLECHAR buf[] = {'O', 'l', 0xE9, 0x2022, 'O', 'l', 0xE9, '\n', 0};
 	OutputDebugString(buf);
+
+	// Restore stdout/stderr
+
+	std::cout.flush();
+	std::cerr.flush();
+	fflush(stdout);
+	fflush(stderr);
+
+	dup2(saved_stdout, STDOUT_FILENO);
+	dup2(saved_stderr, STDERR_FILENO);
+
+	// Check results
+
+	std::string expected_out;
+	std::string expected_err(
+		"Wide\xe2\x80\xa2Wide\xe2\x80\xa2Wide\n"
+		"Narrow\xe2\x80\xa2Narrow\xe2\x80\xa2Narrow\n"
+		"Ol\xc3\xa9\xe2\x80\xa2Ol\xc3\xa9\n");
+
+	try
+	{
+		equal("stdout", outfile, expected_out) || ++failures;
+		equal("stderr", errfile, expected_err) || ++failures;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+		++failures;
+	}
+
+	return failures;
+}
+
+inline void throw_syserr(std::string call)
+{
+	throw std::runtime_error(call + std::string(" failed: ") + sys_errlist[errno]);
+}
+
+std::string read_all(FILE* f)
+{
+	rewind(f);
+
+	struct stat st;
+	if (fstat((fileno(f)), &st) != 0)
+		throw_syserr("fstat");
+
+	std::vector<char> buf(st.st_size);
+	if (fread(&buf[0], 1, buf.size(), f) != buf.size())
+		throw_syserr("fread");
+
+	return std::string(buf.begin(), buf.end());
+}
+
+std::string escape(std::string s)
+{
+	std::ostringstream result;
+	result << std::setbase(16) << std::setfill('0');
+	for (std::string::iterator p = s.begin(); p != s.end(); ++p)
+		if (*p == '\\')
+			result << '\\' << *p;
+		else if (std::isprint(*p) || *p == '\n')
+			result << *p;
+		else
+			result << "\\x" << std::setw(2) << int(static_cast<unsigned char>(*p));
+	return result.str();
+}
+
+bool equal(std::string name, FILE* f, std::string expected)
+{
+	std::string actual = read_all(f);
+
+	if (actual != expected)
+		std::cerr << name << " differs from expected result:\n\n"
+			<< escape(actual) << "\nshould be\n\n" << escape(expected) << "\n";
+
+	return actual == expected;
 }
